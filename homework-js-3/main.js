3d567c0b81f904b76c925ee01c89bf26b1f24160
egortudeikin <channelfunny936@gmail.com>
2026-08-18 13:10:54 +0400

    Add files via upload

---
+++ homework-js-3/main.js
@@ -0,0 +1,17 @@
+// Задача 1
+for (let i = 1; i >= 1 && i <= 20; i++) {
+  if (i % 4 === 0) {
+    continue;
+  }
+
+  console.log(i);
+}
+
+// Задача 2
+const j = +prompt("Введите число", "");
+let k;
+for (let z = 1; z <= j; z++) {
+  k = j * z;
+}
+
+console.log(k);
